fb43a3c34243dba68112f5581035ff8035038f72
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2020-10-09 00:01:19 -0300

    Create Class TransactionTDO

--- src/main/java/com/financial/model/TransactionTDO.java
+++ src/main/java/com/financial/model/TransactionTDO.java
@@ -3,12 +3,12 @@ package com.financial.model;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.hateoas.RepresentationModel;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sun.istack.NotNull;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,22 +19,23 @@ import lombok.Setter;
 @EqualsAndHashCode(callSuper = false)
 public class TransactionTDO extends RepresentationModel<TransactionTDO>{
 	
-private Long id;
+	private Long id;
 	
-	@NotNull()
+	@javax.validation.constraints.NotNull(message = "Nsu cannot be null")
+	@Length(min = 6,message = "Nsu must contain at least 6 characters")
 	private String nsu;
 
 	private String authorizationNumber;
 	
 	@DateTimeFormat(iso = ISO.DATE_TIME)
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-	@NotNull()
+	@javax.validation.constraints.NotNull(message = "TransactionDate cannot be null")
 	private Date transactionDate;
 	
-	@NotNull()
+	@javax.validation.constraints.NotNull(message = "Amount cannot be null")
 	private BigDecimal amount;
 	
-	@NotNull()
+	@javax.validation.constraints.NotNull(message = "Type cannot be null")
 	private TransactionTypeEnum type;
 
 
